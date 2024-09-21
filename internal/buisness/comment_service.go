88ad8a8b5e22c7b6231a5990f6be44baa9d3b8b3
Vladislav <votanak@evrone.com>
2024-09-21 07:05:45 +0300

    add comments logic

--- internal/buisness/comment_service.go
+++ internal/buisness/comment_service.go
@@ -49,9 +49,34 @@ func (s *CommentService) GetCommentByID(id int) (*models.Comment, error) {
 	return &comment, nil
 }
 
-func (s *CommentService) CreateComment(comment *models.Comment) error {
-	_, err := s.db.Query("INSERT INTO comments(todo_id, content, author) VALUES (?,?,?)", comment.TodoID, comment.Content, comment.Author)
+func (s *CommentService) CreateComment(comment *models.Comment) (int, string, error) {
+	var id int
+	var createdAt string
+
+	// Используем QueryRow для получения результата
+	err := s.db.QueryRow(
+		"INSERT INTO comments(todo_id, content, author) VALUES ($1, $2, $3) RETURNING id, created_at",
+		comment.TodoID, comment.Content, comment.Author,
+	).Scan(&id, &createdAt)
+
+	return id, createdAt, err
+}
+
+func (s *CommentService) DeleteCommentByID(id int) error {
+	_, err := s.db.Exec("DELETE FROM comments WHERE id = ?", id)
 	return err
 }
 
-func (s *CommentService) DeleteCommentByID()
+func (s *CommentService) UpdateCommentByID(comment *models.Comment) error {
+	err := s.db.QueryRow("UPDATE comments SET todo_id = ?, content = ?, author = ? WHERE id = ? RETURNING created_at",
+		comment.TodoID, comment.Content, comment.Author, comment.ID).Scan(&comment.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return errors.New("comment not found")
+		}
+		return err
+	}
+
+	return nil
+}
--- pkg/api/routers.go
+++ pkg/api/routers.go
@@ -11,17 +11,25 @@ func SetupRouter(db *storage.Database) *gin.Engine {
 	router := gin.Default()
 
 	todoService := buisness.NewTodoService(db)
+	commentService := buisness.NewCommentService(db)
 
 	router.Use(func(c *gin.Context) {
 		c.Set("todoService", todoService)
+		c.Set("commentService", commentService)
 		c.Next()
 	})
 
 	router.GET("/todos", GetTodos)
-	router.POST("/todos", CreateTodo)
-	router.GET("/todos/:id", GetTodoById)
-	router.DELETE("/todos/:id", DeleteTodo)
-	router.PUT("/todos/:id", UpdateTodo)
+	router.POST("/todo", CreateTodo)
+	router.GET("/todo/:id", GetTodoById)
+	router.DELETE("/todo/:id", DeleteTodo)
+	router.PUT("/todo/:id", UpdateTodo)
+
+	router.GET("/comments/:todo_id", GetCommentsByTodoID)
+	router.GET("/comment/:id", GetCommentByID)
+	router.POST("/comment", CreateComment)
+	router.DELETE("/comment/:id", DeleteComment)
+	router.PUT("/comment/:id", UpdateComment)
 
 	return router
 }
